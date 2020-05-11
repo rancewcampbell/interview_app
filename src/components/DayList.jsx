49149822509f8e20fc0081533a5177df9267407b
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-11 15:31:59 -0600

    basic daylist complete

---
+++ src/components/DayList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+const DayList = (props) => {
+  const { days, setDay } = props;
+  return (
+    <ul>
+      {days.map((day) => {
+        return (
+          <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            selected={day.name === props.day}
+            setDay={setDay}
+          />
+        )
+      })}
+    </ul>
+  );
+};
+
+export default DayList;
