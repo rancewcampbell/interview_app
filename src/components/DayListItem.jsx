655134b2621b202bb7c0a4f126176130f21c96e0
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-11 14:40:16 -0600

    add conditional for spots available message

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,17 +4,25 @@ import classNames from 'classnames';
 
 const DayListItem = (props) => {
   const { name, spots, selected, setDay } = props;
+  let message;
   const dayClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': selected,
     'day-list__item--full': spots === 0
-  })
+  });
+  if (spots === 0) {
+    message = 'no spots remaining';
+  } else if (spots === 1) {
+    message = '1 spot remaining';
+  } else {
+    message = `${spots} spots remaining`;
+  }
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{ name }</h2> 
-      <h3 className="text--light">{ spots } spots remaining</h3>
+      <h3 className="text--light">{message}</h3>
     </li>
   );
 };
 
-export default DayListItem
+export default DayListItem;
