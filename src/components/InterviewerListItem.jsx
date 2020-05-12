964daafb034575c61b8eb189ef6ef40653f462a0
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-12 08:32:20 -0600

    create InterviewerListItem component

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import classNames from 'classnames';
+import './InterviewerListItem.scss';
+
+const InterviewerListItem = (props) => {
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const interviewersClass = classNames({
+    'interviewers__item': true,
+    'interviewers__item--selected': selected, 
+  });
+  const interviewersImg = classNames({
+    'interviewers__item-image': true,
+    'interviewers__item-selected-image': selected
+  });
+
+  return (
+    <li className={interviewersClass} onClick={setInterviewer}>
+      <img
+        className={interviewersImg}
+        src={avatar}
+        alt={name}
+      />
+      {name}
+    </li>
+  )
+};
+
+export default InterviewerListItem;
