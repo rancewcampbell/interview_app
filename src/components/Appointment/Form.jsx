ee3dd90c3fe57515c98374ea6a19372f223eb3bc
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-12 16:54:54 -0600

    factor out onSave function

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -17,6 +17,8 @@ const Form = (props) => {
     onCancel();
   };
 
+  const save = () => onSave(studentName, interviewerId);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -35,7 +37,7 @@ const Form = (props) => {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => onSave(studentName, interviewerId)} confirm>Save</Button>
+          <Button onClick={save} confirm>Save</Button>
         </section>
       </section>
     </main>
