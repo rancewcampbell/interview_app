2e1353cc35341b19a8f55e984701f5b0d7c1b6dd
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-12 14:58:28 -0600

    create Error component of Appointment

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,3 +146,4 @@ storiesOf("DayListItem", module)
     .add("Show", () => <Show student="Lydia Miller-Jones" interviewer="Sylvia Palmer" onDelete={action("onDelete")} onEdit={action("onEdit")} />)
     .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
     .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
