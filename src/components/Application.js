b160b1ae51d4f048f6ef72a24dee2008b3b1e823
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-13 14:12:42 -0600

    add api fetch for appointment data and render data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,63 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Appointment from './Appointment/index';
 import DayList from './DayList';
+import { getAppointmentsForDay } from '../helpers/selectors';
 import 'components/Application.scss';
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Michael Fox",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm"
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Robin Williams",
-      interviewer: { 
-        id: 5, 
-        name: "Sven Jones", 
-        avatar: "https://i.imgur.com/twYrpay.jpg" }
-    }
-  }
-
-];
 const Application= () => {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+  // const { day, days, appointments } = state;
+  const setDay = day => setState({...state, day});
+
+  const appointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then((res) => {
-        setDays(res.data);
-      });
+    const promise1 = axios.get('http://localhost:8001/api/days');
+    const promise2 = axios.get('http://localhost:8001/api/appointments')
+    Promise.all([promise1, promise2])
+      .then(all => {
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+      })
   }, []);
 
   return (
@@ -72,8 +36,8 @@ const Application= () => {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList 
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         />
       </nav>
