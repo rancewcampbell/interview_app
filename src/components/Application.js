50ddbebb6d1f4672765066a5ff64e0358d72261c
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-15 14:36:31 -0600

    separate main state logic into useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,70 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useApplicationData from '../hooks/useApplicationData';
 import Appointment from './Appointment/index';
 import DayList from './DayList';
 import { getAppointmentsForDay, getInterviewersForDay } from '../helpers/selectors';
 import 'components/Application.scss';
 
 const Application= () => {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: []
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
   const { day, days } = state;
-  const setDay = day => setState({...state, day});
   const appointments = getAppointmentsForDay(state, day);
   const interviewersForDay = getInterviewersForDay(state, day);
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
-    return (
-      axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
-        .then(() => {
-          setState(state => {return {...state, appointments}});
-        })
-    )
-  }
-
-  const cancelInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return (
-      axios.delete(`http://localhost:8001/api/appointments/${id}`)
-        .then(() => {
-          setState(state => {return {...state, appointments}})
-        }) 
-    )
-  };
-
-  useEffect(() => {
-    const promise1 = axios.get('http://localhost:8001/api/days');
-    const promise2 = axios.get('http://localhost:8001/api/appointments');
-    const promise3 = axios.get('http://localhost:8001/api/interviewers');
-    Promise.all([promise1, promise2, promise3])
-      .then(all => {
-        setState(state => (
-          {...state,
-            days: all[0].data,
-            appointments: all[1].data,
-            interviewers: all[2].data
-          }))
-      })
-  }, []);
+  
   return (
     <main className="layout">
       <section className="sidebar">
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const useApplicationData = () => {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: []
+  });
+  const setDay = day => setState({...state, day});
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    return (
+      axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+        .then(() => {
+          setState(state => {return {...state, appointments}});
+        })
+    )
+  }
+
+  const cancelInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return (
+      axios.delete(`http://localhost:8001/api/appointments/${id}`)
+        .then(() => {
+          setState(state => {return {...state, appointments}})
+        }) 
+    )
+  };
+
+  useEffect(() => {
+    const promise1 = axios.get('http://localhost:8001/api/days');
+    const promise2 = axios.get('http://localhost:8001/api/appointments');
+    const promise3 = axios.get('http://localhost:8001/api/interviewers');
+    Promise.all([promise1, promise2, promise3])
+      .then(all => {
+        setState(state => (
+          {...state,
+            days: all[0].data,
+            appointments: all[1].data,
+            interviewers: all[2].data
+          }))
+      })
+  }, []);
+
+  return { bookInterview, setDay, cancelInterview, state }
+};
+
+export default useApplicationData;
