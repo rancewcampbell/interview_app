8e7c9c988c1d82cb8e3bbeb9158b1a560526a5c7
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-15 17:27:34 -0600

    refactor updateSpots w/ filter method

--- src/helpers/updateSpots.js
+++ src/helpers/updateSpots.js
@@ -1,15 +1,9 @@
 const updateSpots = (state, appointments) => {
   const originalInterview = state.days.find(d => d.name === state.day);
-  let spots = 5;
-  for (const item of originalInterview.appointments) {
-    if (appointments[item].interview) {
-      spots -= 1
-    }
-  }
   const index = state.days.indexOf(originalInterview);
-  const newInterview = {...state.days[index]}
-  newInterview.spots = spots;
-  const days = [...state.days,];
+  const spots = originalInterview.appointments.filter(el => !appointments[el].interview)
+  const newInterview = {...state.days[index], spots: spots.length}
+  const days = [...state.days];
   days[index] = newInterview
   return days;
 };
