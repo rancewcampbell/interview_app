b546b81ab7e1fdf61cafba5e101d546624cb2dcc
Rance Campbell <rancecampbell@Rances-MBP.hitronhub.home>
2020-05-20 16:43:23 -0600

    add websocket mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,7 @@
-import "@testing-library/react/cleanup-after-each";
-import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/react/cleanup-after-each';
+import '@testing-library/jest-dom/extend-expect';
+import { WebSocket, Server } from 'mock-socket';
+
+const mockServer = new Server(process.env.REACT_APP_WEBSOCKET_URL);
+
+global.WebSocket = WebSocket;
